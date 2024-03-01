a52998e317259fe82a923635386d3243e370b445
chunkypenguin <abeteta@ucsc.edu>
2024-02-29 23:55:46 -0800

    Alex's branch commit(hop stuff)

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,6 +4,15 @@ class Play extends Phaser.Scene {
     }
 
     init() {
+        // frog
+        this.frogVelocity = 700
+        this.frogMaxVelocity = 800
+        this.frogBounce = 0.5
+
+        // hop points
+        this.hopPoint = new Phaser.Math.Vector2()
+        this.canHop = true
+
          // rat
          this.ratSpeed = -150
          this.ratSpawnDelay = 1000
@@ -13,6 +22,19 @@ class Play extends Phaser.Scene {
     }
 
     create(){
+        // set up cursor keys
+        cursors = this.input.keyboard.createCursorKeys()
+
+        //creation of frog and its properties
+        this.frog = this.physics.add.sprite(250, 375).setOrigin(0.5).setScale(0.5)
+        this.frog.setCollideWorldBounds(true)
+        this.frog.setBounce(this.frogBounce)
+        this.frog.setImmovable()
+        this.frog.setMaxVelocity(0, this.frogMaxVelocity )
+        this.frog.setDragY(this.frogDragY)
+        this.frog.setDepth(1)
+        this.frog.destroyed = false 
+
         // set up rat group
         this.ratGroup = this.add.group({
             runChildUpdate: true // make sure update runs on group children
@@ -20,6 +42,43 @@ class Play extends Phaser.Scene {
         this.time.delayedCall(this.ratSpawnDelay, () => { 
             this.addRat()
         })
+    }
+
+    update() {
+        // movement inputs
+        if(Phaser.Input.Keyboard.JustDown(cursors.down)) {
+            if(this.frog.y < 525 && this.canHop) {
+                //hop to test
+                this.hopPoint.x = this.frog.x
+                this.hopPoint.y = this.frog.y + 150
+                this.physics.moveToObject(this.frog, this.hopPoint, this.frogVelocity)
+                this.canHop = false
+            }
+        }
+        else if(Phaser.Input.Keyboard.JustDown(cursors.up)) {
+            if(this.frog.y > 75 && this.canHop) {
+                //hop to test
+                this.hopPoint.x = this.frog.x
+                this.hopPoint.y = this.frog.y - 150
+                this.physics.moveToObject(this.frog, this.hopPoint, this.frogVelocity)
+                this.canHop = false
+            }
+        }
+
+          // check to see if frog has reached hop point
+          const tolerance = 4;
+
+          const distance = Phaser.Math.Distance.BetweenPoints(this.frog, this.hopPoint)
+  
+          if (this.frog.body.speed > 0)
+          {
+  
+              if (distance < tolerance)
+              {
+                  this.frog.body.reset(this.hopPoint.x, this.hopPoint.y)
+                  this.canHop = true
+              }
+          }
     }
 
     addRat() {
